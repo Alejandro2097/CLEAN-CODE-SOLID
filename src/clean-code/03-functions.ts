f1e7ce75da0a93ff5e3fb3e8f898207d8532def7
Alejandro2097 <alejo12381@hotmail.es>
2023-01-24 19:45:53 -0500

    refactoring to improve even more the function

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -41,12 +41,10 @@
     
     const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
         let result: number;
-        if ( isDead ) {
-            result = 1500;
-        } 
-        if(isSeparated){
-            return 2500;
-        }
+        if ( isDead ) result = 1500;
+
+        if(isSeparated) return 2500;
+
         return (isRetired) ? 3000 : 4000;
     }
 
