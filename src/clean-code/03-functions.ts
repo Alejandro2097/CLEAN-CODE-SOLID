3ccd37f7a3d9b5cef6aa12d2e0a4719a6e5dbbf7
Alejandro2097 <alejo12381@hotmail.es>
2023-01-24 19:44:31 -0500

    refactoring function

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -39,6 +39,15 @@
     }
 
     
-
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result: number;
+        if ( isDead ) {
+            result = 1500;
+        } 
+        if(isSeparated){
+            return 2500;
+        }
+        return (isRetired) ? 3000 : 4000;
+    }
 
 })();
