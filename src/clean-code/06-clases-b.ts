37e86f2f928c7b8e9b6d23099c7fcf40fbb51660
Alejandro2097 <alejo12381@hotmail.es>
2023-02-08 20:20:55 -0500

    responsabilidad unicA

--- src/clean-code/06-clases-b.ts
+++ src/clean-code/06-clases-b.ts
@@ -4,28 +4,50 @@
 
     type Gender = 'M'|'F';
 
+    interface PersonProps {
+        birthdate : Date;
+        gender    : Gender;
+        name      : string;
+    }
+
     class Person {
-        constructor(
-            public name: string, 
-            public gender: Gender, 
-            public birthdate: Date
-        ){}
+        public birthdate: Date;
+        public gender   : Gender;
+        public name     : string;
+
+        constructor({ name, gender, birthdate }: PersonProps ){
+            this.name      = name;
+            this.gender    = gender;
+            this.birthdate = birthdate;
+        }
     }
 
 
+    interface UserProps {
+        birthdate : Date;
+        email     : string;
+        gender    : Gender;
+        name      : string;
+        role      : string;
+    }
+
     class User extends Person {
         
+        public email: string;
+        public role : string;
         public lastAccess: Date;
 
-        constructor(
-            public email: string,
-            public role: string,
-            name: string,
-            gender: Gender,
-            birthdate: Date,
-        ) {
-            super( name, gender, birthdate );
+        constructor({
+            birthdate,
+            email,
+            gender,
+            name,
+            role,
+        }: UserProps ) {
+            super({ name, gender, birthdate });
             this.lastAccess = new Date();
+            this.email = email;
+            this.role  = role;
         }
 
         checkCredentials() {
@@ -34,30 +56,46 @@
     }
 
 
+    interface UserSettingsProps {
+        birthdate        : Date;
+        email            : string;
+        gender           : Gender;
+        lastOpenFolder   : string;
+        name             : string;
+        role             : string;
+        workingDirectory : string;
+    }
+
     class UserSettings extends User {
-        constructor(
-            public workingDirectory: string,
-            public lastOpenFolder  : string,
-            email                  : string,
-            role                   : string,
-            name                   : string,
-            gender                 : Gender,
-            birthdate              : Date
-        ) {
-            super(email, role, name, gender, birthdate );
+
+        public workingDirectory: string;
+        public lastOpenFolder  : string;
+
+        constructor({
+            workingDirectory,
+            lastOpenFolder,
+            email,
+            role,
+            name,
+            gender,
+            birthdate,
+        }: UserSettingsProps ) {
+            super({ email, role, name, gender, birthdate });
+            this.workingDirectory = workingDirectory;
+            this.lastOpenFolder   = lastOpenFolder;
         }
     }
 
 
-    const userSettings = new UserSettings(
-        '/usr/home',
-        '/home',
-        'fernando@google.com',
-        'Admin',
-        'Fernando',
-        'M',
-        new Date('1985-10-21')
-    );
+    const userSettings = new UserSettings({
+        birthdate: new Date('1985-10-21'),
+        email: 'fernando@google.com',
+        gender: 'M',
+        lastOpenFolder: '/home',
+        name: 'Fernando',
+        role: 'Admin',
+        workingDirectory: '/usr/home',
+    });
 
     console.log({ userSettings });
 
