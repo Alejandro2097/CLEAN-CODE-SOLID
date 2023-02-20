129d7d057f44fdd865810a6ab7c250fd4dc4e5f4
Alejandro2097 <alejo12381@hotmail.es>
2023-02-19 22:26:09 -0500

    start ocp

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -23,24 +23,32 @@
     // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
     // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
     class ProductBloc {
+        private productService: ProductService;
+        private mailer: Mailer;
+
+        constructor(productService: ProductService, mailer: Mailer) {
+            this.productService = productService;
+            this.mailer = mailer;
+        }
     
         loadProduct( id: number ) {
             // Realiza un proceso para obtener el producto y retornarlo
-            console.log('Producto: ',{ id, name: 'OLED Tv' });
+            this.productService.getProduct(id);
         }
     
         saveProduct( product: Product ) {
             // Realiza una petición para salvar en base de datos 
-            console.log('Guardando en base de datos', product );
+            this.productService.saveProduct(product);
         }
     
         notifyClients() {
-            console.log('Enviando correo a los clientes');
+            this.mailer.sendEmail(['eduardo@google.com'], 'to-clients');
         }
     
     
     }
     
+
     class CartBloc {
         private itemsInCart: Object[] = []
         addToCart(productId: number ) {
@@ -49,7 +57,10 @@
         }
     }
 
-    const productBloc = new ProductBloc();
+
+    const productService = new ProductService();
+    const mailer = new Mailer();
+    const productBloc = new ProductBloc(productService, mailer);
     const cartBloc = new CartBloc()
 
     productBloc.loadProduct(10);
