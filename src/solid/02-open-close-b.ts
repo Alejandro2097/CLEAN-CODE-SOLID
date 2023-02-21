858bb59ee14cdd5c5e4dce8b214958fc588a452b
Alejandro2097 <alejo12381@hotmail.es>
2023-02-20 21:39:40 -0500

    implementing ocp

--- src/solid/02-open-close-b.ts
+++ src/solid/02-open-close-b.ts
@@ -1,28 +1,32 @@
+import { HttpClient } from './03-open-close-c';
+
 // Hay que agregar la dependencia de axios ```yarn add axios```
-import axios from 'axios';
+
 
 export class TodoService { 
-
+    constructor( private http: HttpClient ){}
     async getTodoItems() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos/');
+        const { data } = await this.http.get('https://jsonplaceholder.typicode.com/todos/');
         return data;
     }
 }
 
 
 export class PostService {
+    constructor( private http: HttpClient ){}
 
     async getPosts() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const { data } = await this.http.get('https://jsonplaceholder.typicode.com/posts');
         return data;
     }
 }
 
 
 export class PhotosService {
+    constructor( private http: HttpClient ){}
 
     async getPhotos() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/photos');
+        const { data } = await this.http.get('https://jsonplaceholder.typicode.com/photos');
         return data;
     }
 
--- src/solid/03-open-close-c.ts
+++ src/solid/03-open-close-c.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 export class HttpClient {
     async get(url: string) {
         const  { data, status  } = await axios.get(url);
-        console.log({status});
         return { data, status }
     }
 
